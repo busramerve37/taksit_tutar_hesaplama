42f0de760d3813be6d1edbceded7810e52c3219c
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-10-02 16:42:31 +0300

    Add files via upload

---
+++ button.ornek.16/button.ornek.16/button.ornek.16/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace button.ornek._16
+{
+    partial class lblFiyat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTaksit = new System.Windows.Forms.Label();
+            this.lblTaksitTutar = new System.Windows.Forms.Label();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fiyat:";
+            // 
+            // lblTaksit
+            // 
+            this.lblTaksit.AutoSize = true;
+            this.lblTaksit.Location = new System.Drawing.Point(29, 86);
+            this.lblTaksit.Name = "lblTaksit";
+            this.lblTaksit.Size = new System.Drawing.Size(39, 13);
+            this.lblTaksit.TabIndex = 1;
+            this.lblTaksit.Text = "Taksit:";
+            // 
+            // lblTaksitTutar
+            // 
+            this.lblTaksitTutar.AutoSize = true;
+            this.lblTaksitTutar.Location = new System.Drawing.Point(108, 200);
+            this.lblTaksitTutar.Name = "lblTaksitTutar";
+            this.lblTaksitTutar.Size = new System.Drawing.Size(67, 13);
+            this.lblTaksitTutar.TabIndex = 2;
+            this.lblTaksitTutar.Text = "Taksit Tutar:";
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(85, 50);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(100, 20);
+            this.txtFiyat.TabIndex = 3;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(297, 146);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 79);
+            this.btnHesapla.TabIndex = 4;
+            this.btnHesapla.Text = "HESAPLA";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(85, 86);
+            this.trackBar1.Maximum = 12;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(177, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 1;
+            // 
+            // lblFiyat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 397);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.lblTaksitTutar);
+            this.Controls.Add(this.lblTaksit);
+            this.Controls.Add(this.label1);
+            this.Name = "lblFiyat";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTaksit;
+        private System.Windows.Forms.Label lblTaksitTutar;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ button.ornek.16/button.ornek.16/button.ornek.16/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button.ornek._16
+{
+    public partial class lblFiyat : Form
+    {
+        public lblFiyat()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double fiyat, taksit, taksitutar;
+
+            fiyat = Convert.ToDouble(txtFiyat.Text);
+            taksit = trackBar1.Value;
+
+            taksitutar = fiyat / taksit;
+            lblTaksitTutar.Text = taksitutar.ToString();
+
+
+        }
+    }
+}
